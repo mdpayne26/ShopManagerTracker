0a543a0ab089925df1400b3d9960f8efd42a5fc6
matthew payne <mdpayne26@yahoo.com>
2019-03-26 09:47:41 -0500

    made a work order model with foreign keys to people, trucks and trailers

--- ManagerTracker/Models/Trucks.cs
+++ ManagerTracker/Models/Trucks.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace ManagerTracker.Models
 {
     public class Trucks
     {
+        [Key]
         public int Id { get; set; }
         public string TruckNumber { get; set; }
         public int? Year { get; set; }
---
+++ ManagerTracker/Models/WorkOrders.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ManagerTracker.Models
+{
+    public class WorkOrders
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string PartNumberOrDescription { get; set; }
+        public string PartQuantity { get; set; }
+        public string PartPrice { get; set; }
+        public string RepairDescription { get; set; }
+        [ForeignKey("People")]
+        public int PeopleId { get; set; }
+        public People People { get; set; }
+        [ForeignKey("Trucks")]
+        public int TruckId { get; set; }
+        public Trucks Trucks { get; set; }
+        [ForeignKey("Trailers")]
+        public int TrailerId { get; set; }
+        public Trailers Trailers { get; set; }
+
+
+    }
+}
